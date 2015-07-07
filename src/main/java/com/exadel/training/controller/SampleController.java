c160fecf5efa66a322cb19142a9ddfc21c7263bf
MrArtem <artem6695@mail.ru>
2015-07-07 15:12:57 +0300

    added hello world

---
+++ src/main/java/com/exadel/training/controller/SampleController.java
@@ -0,0 +1,20 @@
+package com.exadel.training.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by HP on 07.07.2015.
+ */
+@Controller
+public class SampleController {
+
+    @RequestMapping(value="/test", method = RequestMethod.GET)
+    @ResponseBody
+    public String Get() {
+        return "Hello World!";
+    }
+
+}
