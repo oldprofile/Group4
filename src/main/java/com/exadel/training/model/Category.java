2843610d52e107d347e479b5814d009e832f6402
klim711 <voknelpush@gmail.com>
2015-07-10 14:06:30 +0300

    adding training, category, state

---
+++ src/main/java/com/exadel/training/model/Category.java
@@ -0,0 +1,51 @@
+package com.exadel.training.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ * Created by Клим on 10.07.2015.
+ */
+@Entity
+@Table(name="categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Training> trainings;
+
+    public Category() {
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Training> getTrainings() {
+        return trainings;
+    }
+
+    public void setTrainings(List<Training> trainings) {
+        this.trainings = trainings;
+    }
+}
