f206013bbc9127c830dcb8c7746e936e204503c0
Anton Dosov <dosantinc@gmail.com>
2015-07-13 11:00:28 +0300

    ToSeeYouNeedServer

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -3,8 +3,8 @@
 // Declare app level module which depends on views, and components
 var app = angular.module('myApp', [
     'ngRoute',
-    'http-auth-interceptor',
     'ui.bootstrap',
+    'http-auth-interceptor',
     'myApp.view1',
     'myApp.view2',
     'myApp.mycourses',
@@ -32,11 +32,8 @@ app.directive('authClass', function() {
       restrict: 'C',
       link: function(scope, elem, attrs) {
           //hide content before auth
+          
           elem.find('#content-outer').hide();
-          
-          
-//        //once Angular is started, remove class:
-//        elem.removeClass('waiting-for-angular');
         
 //        var login = elem.find('#login-holder');
 //        var main = elem.find('#content');
