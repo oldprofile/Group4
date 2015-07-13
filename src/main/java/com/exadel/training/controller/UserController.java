02bdf5ca0c6138867079690681268b46e903b453
MrArtem <artem6695@mail.ru>
2015-07-13 15:52:19 +0300

    create request find list of user by role

--- src/main/java/com/exadel/training/controller/UserController.java
+++ src/main/java/com/exadel/training/controller/UserController.java
@@ -1,11 +1,25 @@
 package com.exadel.training.controller;
 
+import com.exadel.training.model.User;
+import com.exadel.training.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by HP on 13.07.2015.
  */
 @Controller
+@RequestMapping("/user_controller")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/role", method = RequestMethod.POST, consumes = "application/json")
+    public @ResponseBody User get() {
+        return new User();
+    }
 }
