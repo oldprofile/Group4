bca86fa63c3c4ea470f74142f374427b9db56b19
sodaria <so.daria.96@gmail.com>
2015-07-16 15:11:34 +0300

    sms-notification realisation

--- src/main/java/com/exadel/training/notification/sms/WrapperNotificationSMS.java
+++ src/main/java/com/exadel/training/notification/sms/WrapperNotificationSMS.java
@@ -1,7 +1,37 @@
 package com.exadel.training.notification.sms;
 
+import com.twilio.sdk.TwilioRestClient;
+import com.twilio.sdk.TwilioRestException;
+import com.twilio.sdk.resource.factory.MessageFactory;
+import com.twilio.sdk.resource.instance.Account;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by HP on 16.07.2015.
  */
 public class WrapperNotificationSMS {
+    // Account Sid and Token
+    public static final String ACCOUNT_SID = "AC153d280ddafbd38701dcf24ba171b762";
+    public static final String AUTH_TOKEN = "8a006dc2490b141eb7a5826db20856b0";
+
+    TwilioRestClient client;
+    Account account;
+
+    public WrapperNotificationSMS() {
+        this.client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
+        this.account = client.getAccount();
+    }
+
+    public void sendSMS(String phoneNumber, String text) throws TwilioRestException {
+        MessageFactory messageFactory = account.getMessageFactory();
+        List<NameValuePair> messageParams = new ArrayList<NameValuePair>();
+        messageParams.add(new BasicNameValuePair("To", phoneNumber));
+        messageParams.add(new BasicNameValuePair("From", "+1 832-981-3678"));
+        messageParams.add(new BasicNameValuePair("Body", text));
+        messageFactory.create(messageParams);
+    }
 }
