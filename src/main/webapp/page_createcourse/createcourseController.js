fe4df4d24bdc9c8d5a9574a3ba230049d7590763
AAneitchik <aaneitchik@gmail.com>
2015-07-16 13:04:21 +0300

    Create course post

--- src/main/webapp/page_createcourse/createcourseController.js
+++ src/main/webapp/page_createcourse/createcourseController.js
@@ -1,13 +1,11 @@
 angular.module('myApp.createcourse')
 .controller('CreateCourseController', ['$scope', 'getCategories', 'createcourse', function($scope, getCategories, createcourse) {
     $scope.categoriesObj = [];
-    $scope.categories = [];
     $scope.types = [
         {name: 'Internal',
         isInternal: true},
         {name: 'External',
         isInternal: false}];
-    $scope.repeat = ['Once-only', 'Repeating'];
     $scope.languages = ['Russian', 'English'];
     console.log($scope.types);
     
@@ -18,20 +16,11 @@ angular.module('myApp.createcourse')
     $scope.courseInfo.userLogin="user123";
     //////////////////
     
-    $scope.courseInfo.categoryName = "";
-    $scope.courseInfo.category = 0;
-    
     $scope.saveData = function() {
-        var index = $scope.categories.indexOf($scope.courseInfo.categoryName);
-        $scope.courseInfo.category = $scope.categoriesObj[index].id;
         console.log($scope.courseInfo);
         createcourse.createCourse($scope.courseInfo);
     }
     getCategories.success(function(data) {
         $scope.categoriesObj = data;
-        for (var c=0; c<$scope.categoriesObj.length; c++){
-            $scope.categories.push($scope.categoriesObj[c].name);
-        }
-        $scope.courseInfo.category = $scope.categories[0]; 
     });
 }]);
