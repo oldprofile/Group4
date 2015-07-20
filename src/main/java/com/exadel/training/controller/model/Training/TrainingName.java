d7e15bcf1c00f4bd35897ec7bf393f54ab2a1836
klim711 <voknelpush@gmail.com>
2015-07-20 11:43:08 +0300

    check login

---
+++ src/main/java/com/exadel/training/controller/model/Training/TrainingName.java
@@ -0,0 +1,19 @@
+package com.exadel.training.controller.model.Training;
+
+/**
+ * Created by Клим on 19.07.2015.
+ */
+public class TrainingName {
+    private String trainingName;
+
+    public TrainingName() {
+    }
+
+    public String getTrainingName() {
+        return trainingName;
+    }
+
+    public void setTrainingName(String trainingName) {
+        this.trainingName = trainingName;
+    }
+}
