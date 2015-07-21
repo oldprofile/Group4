16aefc9b27700add06933f836e238e64999d1b03
AAneitchik <aaneitchik@gmail.com>
2015-07-21 12:58:26 +0300

    getting courses to approve

--- src/main/webapp/page_admin/adminController.js
+++ src/main/webapp/page_admin/adminController.js
@@ -1,9 +1,14 @@
 angular.module('myApp.admin')
 .controller('AdminController', ['$scope', 'getToApproveList', function($scope, getToApproveList) {
-    $scope.toApproveList=[{name: 'Java', status: 'needs approval'}, {name: 'Angular', status: 'was edited'}, {name: 'Java', status: 'needs approval'}, {name: 'Angular', status: 'was edited'}, {name: 'Java', status: 'needs approval'}, {name: 'Angular', status: 'was edited'}, {name: 'Java', status: 'needs approval'}, {name: 'Angular', status: 'was edited'}, {name: 'Java', status: 'needs approval'}, {name: 'Angular', status: 'was edited'}];
+    $scope.toApproveList=[];
     
     
-    /*getToApproveList.success(function(data) {
+    getToApproveList.success(function(data) {
         $scope.toApproveList = data;
-    });*/
+        console.log($scope.toApproveList);
+    });
 }]);
+
+/*
+{name: 'Java', status: 'needs approval'}, {name: 'Angular', status: 'was edited'}, {name: 'Java', status: 'needs approval'}, {name: 'Angular', status: 'was edited'}, {name: 'Java', status: 'needs approval'}, {name: 'Angular', status: 'was edited'}, {name: 'Java', status: 'needs approval'}, {name: 'Angular', status: 'was edited'}, {name: 'Java', status: 'needs approval'}, {name: 'Angular', status: 'was edited'}
+*/
--- src/main/webapp/page_admin/adminService.js
+++ src/main/webapp/page_admin/adminService.js
@@ -1,6 +1,6 @@
 angular.module('myApp.admin')
 .factory('getToApproveList', ['$http', function($http) {
-  return $http.get('http://localhost:8080/allCategories')
+  return $http.get('http://localhost:8080/training_controller/trainings_for_approve')
             .success(function(data) {
               return data;
             })
