57c7f8df9637eaf5e9940bd94aae5c6085caa19a
MrArtem <artem6695@mail.ru>
2015-07-23 10:44:22 +0300

    create controller for news

--- src/main/java/com/exadel/training/controller/NewsController.java
+++ src/main/java/com/exadel/training/controller/NewsController.java
@@ -1,7 +1,13 @@
 package com.exadel.training.controller;
 
+import com.exadel.training.model.News;
+import com.exadel.training.service.NewsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by HP on 23.07.2015.
@@ -9,4 +15,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/news")
 public class NewsController {
+
+    @Autowired
+    private NewsService userNewsService;
+
+    @RequestMapping(value = "/pages/{pageNumber}", method = RequestMethod.GET)
+    public String getRunbookPage(@PathVariable("pageNumber")Integer pageNumber) {
+        Page<News> page = userNewsService.getNewsPage(pageNumber);
+
+       
+        return "deploymentLog";
+    }
 }
