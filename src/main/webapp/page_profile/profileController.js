172000f51e5a8ce95fe17a3e9f9b74035182f48f
Anton Dosov <dosantinc@gmail.com>
2015-07-24 11:26:12 +0300

    ProfileFeedbacks

--- src/main/webapp/page_profile/profileController.js
+++ src/main/webapp/page_profile/profileController.js
@@ -1,5 +1,6 @@
 angular.module('myApp.browse')
-.controller('ProfileController',['$scope','userService','$routeParams','profileService', function($scope,userService,$routeParams,profileService){
+.controller('ProfileController',['$scope','userService','$routeParams','profileService','$modal', function($scope,userService,$routeParams,profileService,$modal){
+    $scope.isContentLoaded = false;
     var userlogin;  
     $scope.studentFeedbacks = [];
     $scope.coachFeedbacks = [];    
@@ -19,6 +20,7 @@ angular.module('myApp.browse')
     profileService.getUserInfo(userlogin).success(function(data){
       alert("User" + JSON.stringify(data));
       $scope.user = data;
+      $scope.isContentLoaded = true;
       
       profileService.getStudentFeedbacks(data.login).success(function(studentFeedbacks){
         console.log("studentFeedbacks:" + JSON.stringify(studentFeedbacks));
@@ -44,7 +46,77 @@ angular.module('myApp.browse')
     }).error(function(err,status){
       alert(status);
       alert("Can't get user " + userlogin);
-    });                                                     
+    });    
+  
+  
+  
+  $scope.leaveStudentFeedback = function(feedback){
+    
+      var feedbackModalInstance = $modal.open({
+      animation: true,
+      templateUrl: 'page_profile/StudentFeedback.html',
+      controller: 'StudentFeedbackController',
+      size: "lg",
+      resolve: {
+        user : function () {
+          return $scope.user;
+        },
+        feedback: function () {
+          return feedback;
+        }
+      }
+    });
+    
+    feedbackModalInstance.result.then(function (feedback) {
+//      feedback.feedbackerLogin = userService.getUser().login;  
+//      feedbacksService.createTrainingFeedback(feedback);
+      alert("Creating new Student Feedback" + JSON.stringify(feedback));
+    }, function () {
+      //cancel feedback
+    });
+    
+    }
     
     
+}])
+.controller('StudentFeedbackController',['$scope', '$modalInstance','user','feedback',function($scope,$modalInstance,user,feedback){
+  $scope.user = user;
+  
+  if(feedback === undefined){
+    alert("Creating feedback");
+    $scope.isView = false;
+    
+    
+    
+    $scope.feedback = {
+    attendance: true,
+    attitude: true,
+    commSkills: true,
+    motivation: true,
+    focusOnResult: true,
+      
+    assessment: true,
+    level: true,
+    
+      
+    other: "",
+    
+    };
+    
+  } else{
+    alert("Viewing: " + JSON.stringify(feedback));
+    $scope.isView = true;
+    $scope.feedback = feedback;
+  }
+  
+  
+  
+  $scope.ok = function () {
+    
+    $modalInstance.close($scope.feedback);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
 }]);
