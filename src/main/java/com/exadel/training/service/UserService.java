958da7f6a9604087e4d8242a6e3476ed12379bd8
MrArtem <artem6695@mail.ru>
2015-07-29 15:13:02 +0300

    modified selected training

--- src/main/java/com/exadel/training/service/UserService.java
+++ src/main/java/com/exadel/training/service/UserService.java
@@ -27,8 +27,9 @@ public interface UserService {
     List<Training> selectAllTrainingSortedByDateTypeCoachTrue(String login, List<Integer> state);
     List<Training> selectAllTrainingSortedByDateTypeCoachFalse(String login, List<Integer> state);
     List<Training> selectAllTrainingBetweenDatesAndSortedByName(String login, Date from, Date to);
-    List<Date> selectAllDateOfTrainingsBetweenDates(String login, Date from, Date to);
     List<Training> selectAllTrainingAndSortedByName(String login);
+
+    List<Date> selectAllDateOfTrainingsBetweenDates(String login, Date from, Date to);
     List<Date> selectAllDateOfTrainings(String login);
 
     List<User> searchUsersByName(String name);
