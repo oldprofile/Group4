5e4ec422d8ea63af859b832454af3317f5cf4dd4
sodaria <so.daria.96@gmail.com>
2015-08-03 12:05:25 +0300

    message fabric added

---
+++ src/main/java/com/exadel/training/notification/MessageFabric.java
@@ -0,0 +1,28 @@
+package com.exadel.training.notification;
+
+/**
+ * Created by asd on 03.08.2015.
+ */
+public class MessageFabric {
+   public static enum messageType{
+        Canceled,
+        OneHourLeft,
+        OneDayLeft,
+        Race;
+    }
+
+    public static String getMessage(messageType messageType, String trainingName) {
+        switch (messageType){
+            case Canceled:
+                return new String("Sorry, your " + trainingName + " training was canceled.");
+            case OneDayLeft:
+                return new String("One day left before your " + trainingName + " training, please, don't forget!");
+            case OneHourLeft:
+                return new String("Don't forget about your " + trainingName + " training, one hour left!");
+            case Race:
+                return new String("We offer you a place on a " + trainingName + " training. Hurry up and contact us if you ready to take it.");
+        }
+        return "";
+    }
+
+}
