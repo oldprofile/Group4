5c0a671f06ed990fd83972d4d9423a627492a3c4
AAneitchik <aaneitchik@gmail.com>
2015-08-10 20:52:02 +0300

    translation started

--- src/main/webapp/translation.js
+++ src/main/webapp/translation.js
@@ -1,7 +1,23 @@
 angular.module('myApp.translations', ['pascalprecht.translate'], ['$translateProvider', function ($translateProvider) {
 
 	$translateProvider.translations('en_EN', {
-		'TO_APPROVE': 'To Approve'
+		'COACH': 'Coach',
+		'COURSES_NEED_ATTENTION': 'Courses that need your attention',
+		'END_DATE': 'End date:',
+		'GENERATE_STATISTICS': 'Generate statistics',
+		'LISTENER': 'Listener',
+		'NAME': 'Name',
+		'NEWS': 'News',
+		'READ_ALL': 'Read All',
+		'SELECT': 'select',
+		'SELECT_PERSON': 'Select person:',
+		'SELECT_TRAINING': 'Select training:',
+		'SELECT_TYPE': 'Select type:',
+		'START_DATE': 'Start date:',
+		'STATISTICS': 'Statistics',
+		'STATUS': 'Status',
+		'TO_APPROVE': 'To Approve',
+		'TRAINING': 'Training',
 	});
 	// register english translation table
 	$translateProvider.translations('ru_RU', {
