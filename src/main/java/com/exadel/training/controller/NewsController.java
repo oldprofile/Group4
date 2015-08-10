143fe9b837ee24096025d4ea181fcc33f30cbb7c
MrArtem <artem6695@mail.ru>
2015-08-10 14:27:25 +0300

    added request

--- src/main/java/com/exadel/training/controller/NewsController.java
+++ src/main/java/com/exadel/training/controller/NewsController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.async.DeferredResult;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -53,8 +54,10 @@ public class NewsController {
     }
 
     @RequestMapping(value = "/change_all_unread", method = RequestMethod.GET)
-    public  @ResponseBody void changeAllUnread() {
+    public  @ResponseBody void changeAllUnread(HttpServletResponse httpServletResponse) {
         userNewsService.updateAllUnreadToReadNews();
+        httpServletResponse.setStatus(HttpServletResponse.SC_ACCEPTED);
+
     }
 
     private List<News> getLatestNews(Long timestamp) {
